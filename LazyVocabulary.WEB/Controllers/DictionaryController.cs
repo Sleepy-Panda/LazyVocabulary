534ab3eae32b423bede9b9113af92279e62e1546
Sleepy Panda <sleepypanda1995@gmail.com>
2017-03-25 15:33:14 +0300

    Added left menu to site pages; fixed some css fot login/register pages

---
+++ LazyVocabulary.WEB/Controllers/DictionaryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LazyVocabulary.WEB.Controllers
+{
+    public class DictionaryController : Controller
+    {
+        // GET: Dictionary
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
