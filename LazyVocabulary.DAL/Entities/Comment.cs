9071ff0cfda16155060fd9fa0f644cf231bd586d
Sleepy Panda <sleepypanda1995@gmail.com>
2017-03-31 17:53:20 +0300

    Added model for comments

---
+++ LazyVocabulary.DAL/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LazyVocabulary.DAL.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Text { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        public string DictionaryId { get; set; }
+        public virtual Dictionary Dictionary { get; set; }
+    }
+}
